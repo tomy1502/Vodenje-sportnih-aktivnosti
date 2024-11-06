c84e64154877d4c8bee39f14b47079221973d32b
Matija334 <matija.krsnik@protonmail.com>
2024-11-06 18:37:57 +0100

    Added support for google maps and sharing options

--- frontend/src/components/EventDetails.tsx
+++ frontend/src/components/EventDetails.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Event } from "../services/api";
 import { eventDateToSlovenian } from "../modules/functions/eventHelperFunctions";
+import {FaFacebookF, FaTwitter, FaInstagram, FaCopy} from "react-icons/fa";
 
 interface EventDetailsProps {
     event: Event;
 }
 
-const EventDetails: React.FC<EventDetailsProps> = ({event}: EventDetailsProps) => {
+const EventDetails: React.FC<EventDetailsProps> = ({ event }: EventDetailsProps) => {
+    const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+
+    const shareEvent = () => {
+        const shareUrl = `${window.location.origin}/events/${event.id}`;
+        navigator.clipboard.writeText(shareUrl);
+        alert("Povezava dogodka je kopirana v odložišče!");
+    };
+
     return (
         <div className="event-details">
             <div className="form-group">
@@ -27,11 +37,40 @@ const EventDetails: React.FC<EventDetailsProps> = ({event}: EventDetailsProps) =
             <div className="form-group">
                 <label htmlFor="location">Lokacija:</label>
                 <div className="form-control-plaintext">{event.location}</div>
+                <div className="mt-2">
+                    <iframe
+                        title="map"
+                        width="100%"
+                        height="200"
+                        style={{ border: 0 }}
+                        src={`https://www.google.com/maps/embed/v1/place?key=${googleMapsApiKey}&q=${encodeURIComponent(
+                            event.location
+                        )}`}
+                        allowFullScreen
+                    ></iframe>
+                </div>
             </div>
             <br />
             <div className="form-group">
                 <label htmlFor="organizer">Organizator:</label>
                 <div className="form-control-plaintext">{event.organizer}</div>
+            </div>
+            <br />
+            <div className="form-group">
+                <div className="d-flex gap-3">
+                    <a href="#" className="text-primary" title="Copy" onClick={shareEvent}>
+                        <FaCopy size={24}/>
+                    </a>
+                    <a href="#" onClick={(e) => e.preventDefault()} className="text-primary" title="Facebook">
+                        <FaFacebookF size={24}/>
+                    </a>
+                    <a href="#" onClick={(e) => e.preventDefault()} className="text-info" title="Twitter">
+                        <FaTwitter size={24}/>
+                    </a>
+                    <a href="#" onClick={(e) => e.preventDefault()} className="text-danger" title="Instagram">
+                        <FaInstagram size={24}/>
+                    </a>
+                </div>
             </div>
         </div>
     );
